5ed17858faa3409db0c3a1b236102c34047f9156
auma2022 <auma.amwayi@student.moringaschool.com>
2022-06-25 12:18:14 +0300

    commit tree 83b1fc55a44addc2ba8a9aadf2d00f8f1d8f51cc

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // compare the reversed string to the input
+  return word === reversedWord;
+}
 /* 
   Add your pseudocode here
+   function to reverse the input string
+
+  if the reversed string is the same as the input
+    return true
+  else
+    return false
 */
 
 /*
   Add written explanation of your solution here
+  
 */
 
 // You can run `node index.js` to view these console logs
